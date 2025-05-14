bd3a676d8d2d7aad237451763fd68ac118596c83
justdoanything <pure_yong@naver.com>
2025-05-14 23:06:29 +0900

    spotless

--- common/src/main/java/prj/yong/common/configuration/CommonConfiguration.java
+++ common/src/main/java/prj/yong/common/configuration/CommonConfiguration.java
@@ -1,13 +1,12 @@
 package prj.yong.common.configuration;
 
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import prj.yong.common.properties.CommonHmacSha256Properties;
 import prj.yong.common.provider.CommonWhiteIpListProvider;
-
-import java.util.List;
 
 @Configuration
 public class CommonConfiguration {

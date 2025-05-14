41ee495f822cb2a07c0a2f47d6372eddad7d58bd
justdoanything <pure_yong@naver.com>
2025-05-14 23:10:20 +0900

    예제 추가

--- common/src/main/java/prj/yong/common/constants/SwaggerWhiteIpConstants.java
+++ common/src/main/java/prj/yong/common/constants/SwaggerWhiteIpConstants.java
@@ -3,5 +3,7 @@ package prj.yong.common.constants;
 import java.util.List;
 
 public class SwaggerWhiteIpConstants {
-    public static List<String> swaggerWhiteIpList = List.of("");
+    // TODO : SwaggerWebFilter를 통과시킬 IP를 등록합니다. (CIDR 사용 가능)
+    public static List<String> swaggerWhiteIpList =
+            List.of("127.0.0.1/32", "127.0.0.1/16", "127.0.0.1");
 }
